ef9379db0c1cc10cdfe2459094674c3aeea93c9a
Peter Tardif <peter.tardif@gmail.com>
2019-05-22 19:59:54 -0600

    refactored to 3 components and working

--- src/LaptopSummary/LaptopSummary.js
+++ src/LaptopSummary/LaptopSummary.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 
 export default class LaptopSummary extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
-        );
+        const summary = Object.keys(this.props.selected)
+                .map(key => <div className="summary__option" key={key}>
+                <div className="summary__option__label">{key}  </div>
+                <div className="summary__option__value">{this.props.selected[key].name}</div>
+                <div className="summary__option__cost">
+                    { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                        .format(this.props.selected[key].cost) }
+                </div>
+            </div>)
+
+        const total = Object.keys(this.props.selected)
+                .reduce((acc, curr) => acc + this.props.selected[curr].cost, 0);  
+                return (
+                    <div>
+                        <section className="main__summary">
+                            <h3>NEW GREENLEAF 2018</h3>
+                            {summary}
+                            <div className="summary__total">
+                            <div className="summary__total__label">Your Price: </div>
+                            <div className="summary__total__value">
+                            { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                                .format(total) }
+                            </div>
+                            </div>
+                        </section>
+                    </div>
+                );
     }
 }
--- src/TechSpecs/TechSpecs.js
+++ src/TechSpecs/TechSpecs.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
-export default class TechSpecs extends Component {
+export default class TechSpecs extends Component { 
     render() {
+        const features = Object.keys(this.props.features)
+            .map(key => {
+            const options = this.props.features[key].map((item, index) => {
+                const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
+                const featureClass = 'feature__option ' + selectedClass;
+                return (
+                <li key={index} className="feature__item">
+                    <div 
+                        className={featureClass}
+                        onClick={e => this.props.onUpdateFeature(key, item)}>
+                        { item.name }
+                        ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                            .format(item.cost) })
+                    </div>
+                </li>
+                )
+            });
+
+      return <div className="feature" key={key}>
+        <div className="feature__name">{key}</div>
+        <ul className="feature__list">
+          { options }
+        </ul>
+      </div>
+    });
         return (
             <div>
-                
+               <section className="main__form">
+                    <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+                    { features }
+                </section> 
             </div>
         );
     }
